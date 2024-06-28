9650ccc33fe15eefbcbf8c4549437a267294d3c7
Salvador Pascual <salva-jose@hotmail.com>
2024-06-28 23:17:49 +0200

    endpoint post

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -4,4 +4,24 @@ const router = express.Router();
 
 router.post("/api/users/login");
 
+//Endpoint para crear un post
+router.post("/posts", async (req, res) => {
+	try {
+		const { text, isPrivate } = req.body;
+		let mediaPath = null;
+
+		//verificar archivos multimedia en la solicitud
+		if (req.files && req.files.media) {
+			const media = req.files.media;
+
+			const uploadPath = `${UPLOADS_DIR}/${media.name}`;
+			await media.mv(uploadPath);
+			mediaPath = uploadPath;
+		}
+		const post = await PostController.createPost({ text, isPrivate, mediaPath });
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+});
+
 export default router;
