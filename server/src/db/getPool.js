9c14fee9e141b293ebfea7f98fe3198f8ad8b845
Saul Pinedo <sxul.95@gmail.com>
2024-07-03 17:41:30 +0200

    endpoint creacion de hermandad

--- server/src/db/getPool.js
+++ server/src/db/getPool.js
@@ -4,22 +4,21 @@ import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from "../../env.js";
 let pool;
 
 const getPool = async () => {
-	try {
-		if (!pool) {
-			// Crear un pool de conexión inicial sin especificar la base de datos.
-			pool = mysql.createPool({
-				host: DB_HOST,
-				user: DB_USER,
-				password: DB_PASSWORD,
-				database: DB_NAME,
-			});
-		}
-		
+  try {
+    if (!pool) {
+      // Crear un pool de conexión inicial sin especificar la base de datos.
+      pool = mysql.createPool({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+      });
+    }
 
-		return pool;
-	} catch (err) {
-		console.error(err);
-	}
+    return pool;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export default getPool;
--- server/src/models/guilds/insertGuildModel.js
+++ server/src/models/guilds/insertGuildModel.js
@@ -1,21 +1,21 @@
-import getPool from "../../db/getPool";
-import { v4 as uuid } from "uuid";
+import uuid4 from "uuid4";
+import getPool from "../../db/getPool.js";
+import { guildlALreadyRegisterError } from "../../services/errorService.js";
 
 const insertGuildModel = async (name, avatar, description, userId) => {
-	let pool = getPool();
-	let [guilds] = await pool.query("SELECT id FROM guild WHERE name = ?", [name]);
+  let pool = await getPool();
+  let [guilds] = await pool.query("SELECT id FROM guilds WHERE name = ?", [
+    name,
+  ]);
 
-	if (guilds.length > 0) guildlALreadyRegisterError();
+  if (guilds.length > 0) guildlALreadyRegisterError();
 
-	// insertar el usario
-	const newGuild = await pool.query(`INSERT INTO guilds(id, name, avatar, description, userId) VALUES(?, ?, ?, ?, ?)`, [
-		uuid(),
-		name,
-		avatar,
-		description,
-		userId,
-	]);
-	return newGuild;
+  // insertar el usario
+  const newGuild = await pool.query(
+    `INSERT INTO guilds(id, name, avatar, description, userId) VALUES(?, ?, ?, ?, ?)`,
+    [uuid4(), name, avatar, description, userId]
+  );
+  return [newGuild];
 };
 
 export default insertGuildModel;
