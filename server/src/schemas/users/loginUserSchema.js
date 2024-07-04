234a6ebf6f650d4bc91065c4b5a678b6b9ed6f20
Tomás Guijarro Ferrandiz <tomasguijarroferrandiz@gmail.com>
2024-07-04 17:14:47 +0200

    error fix

--- server/src/schemas/users/loginUserSchema.js
+++ server/src/schemas/users/loginUserSchema.js
@@ -1,12 +1,12 @@
-import joi from 'joi';
+import joi from "joi";
 
 // Importamos los mensajes de error personalizados.
-import joiErrorMessages from '../joiErrorMessages.js';
+import joiErrorMessages from "../joiErrorMessages.js";
 
 // Creamos el esquema de Joi.
 const loginUserSchema = joi.object({
-    password: joi.string().required().messages(joiErrorMessages),
-    username: joi.string().username().required().messages(joiErrorMessages),
+	password: joi.string().required().messages(joiErrorMessages),
+	username: joi.string().email().required().messages(joiErrorMessages),
 });
 
 export default loginUserSchema;
