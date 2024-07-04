9f54d6a142b228afa6ae785e54fdd249f6faa116
Tomás Guijarro Ferrandiz <tomasguijarroferrandiz@gmail.com>
2024-07-04 17:38:33 +0200

    PUNTO JOTA ESE

--- server/src/models/users/resetPasswordModel.js
+++ server/src/models/users/resetPasswordModel.js
@@ -1,20 +1,17 @@
-import getPool from "../../db/getPool";
+import getPool from "../../db/getPool.js";
 import bcrypt from "bcrypt";
 
 const resetPasswordModel = async (email, recoverPassCode, newPassword) => {
-  const pool = await getPool();
-  const user = await selectUserModel(email);
+	const pool = await getPool();
+	const user = await selectUserModel(email);
 
-  if (!user || user.recoverPassCode !== recoverPassCode) {
-    recoveryCodeError();
-  }
+	if (!user || user.recoverPassCode !== recoverPassCode) {
+		recoveryCodeError();
+	}
 
-  const hashedPass = await bcrypt.hash(newPassword, 10);
+	const hashedPass = await bcrypt.hash(newPassword, 10);
 
-  await pool.query(
-    `UPDATE users SET password = ?, recoverPassCode = null WHERE recoverPassCode = ?`,
-    [hashedPass, recoverPassCode]
-  );
+	await pool.query(`UPDATE users SET password = ?, recoverPassCode = null WHERE recoverPassCode = ?`, [hashedPass, recoverPassCode]);
 };
 
 export default resetPasswordModel;
