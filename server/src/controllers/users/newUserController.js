ddca956d54aaba4bd9571d8e72ea5df316e43014
Tomás Guijarro Ferrandiz <tomasguijarroferrandiz@gmail.com>
2024-07-05 11:12:02 +0200

    bug fix

--- server/src/controllers/users/newUserController.js
+++ server/src/controllers/users/newUserController.js
@@ -1,23 +1,23 @@
 import randomstring from "randomstring";
 import validateSchema from "../../utils/validateSchema.js";
-import {newUserSchema} from "../../schemas/users/index.js";
+import { newUserSchema } from "../../schemas/users/index.js";
 import sendMailUtil from "../../utils/sendEmailUtil.js";
 import { insertUserModel } from "../../models/users/index.js";
 
 //? diarodeviajes.com/api/users/register
 const newUserController = async (req, res, next) => {
-  try {
-    await validateSchema(newUserSchema, req.body);
-    const { username, email, password } = req.body;
+	try {
+		await validateSchema(newUserSchema, req.body);
+		const { username, email, password } = req.body;
 
-    // Validamos el body con Joi.
+		// Validamos el body con Joi.
 
-    // generar el codigo de registro para activar la cuenta
-    const registrationCode = randomstring.generate(30);
+		// generar el codigo de registro para activar la cuenta
+		const registrationCode = randomstring.generate(30);
 
-    // generamos el correo y el cuerpo del correo
-    const emailSubject = "Activa tu cuenta de Raid Manager 📝";
-    const emailBody = `
+		// generamos el correo y el cuerpo del correo
+		const emailSubject = "Activa tu cuenta de Raid Manager 📝";
+		const emailBody = `
     ¡Bienvenid@ ${username}!
 
     Gracias por registrarte en Raid Manager!
@@ -27,24 +27,29 @@ const newUserController = async (req, res, next) => {
     <a href="http://localhost:3000/auth/activate/${registrationCode}">Activar mi cuenta</a>
    `;
 
-    // ya validada, llamamos al modelo para que realice la consulta a la base de datos
-    await insertUserModel(username, email, password, registrationCode);
+		// ya validada, llamamos al modelo para que realice la consulta a la base de datos
+		await insertUserModel(username, email, password, registrationCode);
 
-    // ya guardado el user en base de datos, enviamos correo con el registtrationCode
-    await sendMailUtil(email, emailSubject, emailBody);
+		// ya guardado el user en base de datos, enviamos correo con el registtrationCode
+		await sendMailUtil(email, emailSubject, emailBody);
 
-    const registratioCode = crypto.randomBytes(20).toString("hex");
+		const registratioCode = await crypto.randomBytes(20).toString("hex");
 
-    // una vez enviado el correo con el codigo de registro mandamos respuesta al cliente
-    res.send({
-      status: "ok",
-      message: "Usuario creado correctamente, por favor revisa tu correo ...",
-    });
-  } catch (err) {
-    next(err);
-    // console.log(error);
-    // res.status(500).send("Error en el servidor al crear user");
-  }
+		// una vez enviado el correo con el codigo de registro mandamos respuesta al cliente
+		res.send({
+			status: "ok",
+			message: "Usuario creado correctamente, por favor revisa tu correo ...",
+			data: {
+				username,
+				email,
+				registratioCode,
+			},
+		});
+	} catch (err) {
+		next(err);
+		// console.log(error);
+		// res.status(500).send("Error en el servidor al crear user");
+	}
 };
 
 export default newUserController;
