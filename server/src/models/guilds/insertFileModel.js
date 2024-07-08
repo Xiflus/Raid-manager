ad1525e185889ae3954d8d8ef12012386600d880
Adrián Mora <adrianmoratoledo@hotmail.com>
2024-07-08 13:33:50 +0200

    joi arreglo

--- server/src/models/guilds/insertFileModel.js
+++ server/src/models/guilds/insertFileModel.js
@@ -1,9 +1,9 @@
 import getPool from "../../db/getPool.js";
 import uuid4 from "uuid4";
 
-const insertFileModel = async (fileName, postId) => {
+const insertFileModel = async (fileName, entryId) => {
 	const pool = await getPool();
-	const [file] = await pool.query(`INSERT INTO postfiles (id, filename, postid) VALUES (?, ?, ?)`, [uuid4(), fileName, postId]);
+	const [file] = await pool.query(`INSERT INTO postfiles (id, filename, postid) VALUES (?, ?, ?)`, [uuid4(), fileName, entryId]);
 
 	return file;
 };
