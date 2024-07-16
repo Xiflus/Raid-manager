ffb377e55d23741de2a0f5b27a79150180281a1f
Roberto Panella <tito874@hotmail.com>
2024-07-16 19:23:03 +0200

    nav bar

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+
+const Header = () => {
+	return (
+		<header className="">
+			<div className="">
+				<NavBar />
+			</div>
+		</header>
+	);
+};
+
+export default Header;
