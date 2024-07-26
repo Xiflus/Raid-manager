400ce55cff81f16e4c3dfe034928babda94ea373
saulpinedoc <sxul.95@gmail.com>
2024-07-26 11:40:47 +0200

    archivos modularizados tailwind

--- client/src/pages/CreateGuildPage.jsx
+++ client/src/pages/CreateGuildPage.jsx
@@ -2,6 +2,12 @@ import { useRef } from "react";
 import toast from "react-hot-toast";
 import { createGuildService } from "../../services/guildService.js";
 import { Link } from "react-router-dom";
+import TextInput from "../components/TextInput.jsx";
+import Textarea from "../components/Textarea.jsx";
+import FileInput from "../components/FileInput.jsx";
+import Button from "../components/Button.jsx";
+import PageContainer from "../components/PageContainer.jsx";
+import FormContainer from "../components/FormContainer.jsx";
 
 const CreateGuildPage = () => {
     const guildnameRef = useRef();
@@ -38,81 +44,34 @@ const CreateGuildPage = () => {
     };
 
     return (
-        <div className="bg-black flex flex-col items-center justify-center flex-1 p-4">
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md lg:max-w-lg xl:max-w-xl">
+        <PageContainer>
+            <FormContainer>
                 <h2 className="text-white text-2xl font-bold mb-6 text-center">
                     Creación de Hermandad
                 </h2>
                 <form onSubmit={handleSubmit} className="space-y-6">
-                    <div className="relative">
-                        <label
-                            htmlFor="charactername"
-                            className="text-white block mb-1"
-                        >
-                            Nombre del personaje, GM:
-                        </label>
-                        <input
-                            type="text"
-                            id="charactername"
-                            ref={characterRef}
-                            className="w-full p-3 bg-gray-900 text-white border border-orange-500 rounded-lg placeholder focus:outline-none"
-                            placeholder="Nombre del personaje"
-                            required
-                        />
-                    </div>
-                    <div className="relative">
-                        <label
-                            htmlFor="guildname"
-                            className="text-white block mb-1"
-                        >
-                            Nombre de la hermandad:
-                        </label>
-                        <input
-                            type="text"
-                            id="guildname"
-                            ref={guildnameRef}
-                            className="w-full p-3 bg-gray-900 text-white border border-orange-500 rounded-lg placeholder focus:outline-none"
-                            placeholder="Nombre de la hermandad"
-                            required
-                        />
-                    </div>
-                    <div className="relative">
-                        <label
-                            htmlFor="description"
-                            className="text-white block mb-1"
-                        >
-                            Descripción:
-                        </label>
-                        <textarea
-                            id="description"
-                            ref={descriptionRef}
-                            className="w-full p-3 bg-gray-900 text-white border border-orange-500 rounded-lg placeholder focus:outline-none"
-                            placeholder="Descripción (opcional)"
-                            rows="4"
-                        />
-                    </div>
-                    <div className="relative">
-                        <label
-                            htmlFor="avatar"
-                            className="text-white block mb-1"
-                        >
-                            Avatar:
-                        </label>
-                        <input
-                            type="file"
-                            id="avatar"
-                            ref={avatarRef}
-                            className="w-full text-white border border-orange-500 rounded-lg"
-                        />
-                    </div>
-                    <div>
-                        <button
-                            type="submit"
-                            className="w-full py-3 bg-orange-600 text-white font-bold rounded-lg hover:bg-orange-700 transition-colors"
-                        >
-                            Crear Hermandad
-                        </button>
-                    </div>
+                    <TextInput
+                        id="charactername"
+                        label="Nombre del personaje, GM:"
+                        placeholder="Nombre del personaje"
+                        ref={characterRef}
+                        required
+                    />
+                    <TextInput
+                        id="guildname"
+                        label="Nombre de la hermandad:"
+                        placeholder="Nombre de la hermandad"
+                        ref={guildnameRef}
+                        required
+                    />
+                    <Textarea
+                        id="description"
+                        label="Descripción:"
+                        placeholder="Descripción (opcional)"
+                        ref={descriptionRef}
+                    />
+                    <FileInput id="avatar" label="Avatar:" ref={avatarRef} />
+                    <Button type="submit">Crear Hermandad</Button>
                 </form>
                 <div className="text-center mt-6">
                     <h4 className="text-white">
@@ -126,8 +85,8 @@ const CreateGuildPage = () => {
                         .
                     </h4>
                 </div>
-            </div>
-        </div>
+            </FormContainer>
+        </PageContainer>
     );
 };
 
--- client/src/pages/EditGuildPage.jsx
+++ client/src/pages/EditGuildPage.jsx
@@ -2,101 +2,70 @@ import { useRef } from "react";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
 import { editGuildService } from "../../services/guildService.js";
+import TextInput from "../components/TextInput.jsx";
+import Textarea from "../components/Textarea.jsx";
+import FileInput from "../components/FileInput.jsx";
+import Button from "../components/Button.jsx";
+import PageContainer from "../components/PageContainer.jsx";
+import FormContainer from "../components/FormContainer.jsx";
 
 const EditGuildPage = () => {
-	const guildnameRef = useRef();
-	const descriptionRef = useRef();
-	const avatarRef = useRef();
-	const { guildId } = useParams();
+    const guildnameRef = useRef();
+    const descriptionRef = useRef();
+    const avatarRef = useRef();
+    const { guildId } = useParams();
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		const name = guildnameRef.current.value;
-		let description = descriptionRef.current.value;
-		let avatar = avatarRef.current.files[0];
-		description = description === "" ? null : description;
-		avatar = avatar ? avatar : null;
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const name = guildnameRef.current.value;
+        let description = descriptionRef.current.value;
+        let avatar = avatarRef.current.files[0];
+        description = description === "" ? null : description;
+        avatar = avatar ? avatar : null;
 
-		const formData = new FormData();
-		formData.append("name", name);
-		if (description) formData.append("description", description);
-		if (avatar) formData.append("avatar", avatar);
+        const formData = new FormData();
+        formData.append("name", name);
+        if (description) formData.append("description", description);
+        if (avatar) formData.append("avatar", avatar);
 
-		try {
-			await editGuildService({ guildId, formData });
-			toast.success("Hermandad modificada correctamente");
-			guildnameRef.current.value = "";
-			descriptionRef.current.value = "";
-			avatarRef.current.value = "";
-		} catch (error) {
-			toast.error(error.message);
-		}
-	};
+        try {
+            await editGuildService({ guildId, formData });
+            toast.success("Hermandad modificada correctamente");
+            guildnameRef.current.value = "";
+            descriptionRef.current.value = "";
+            avatarRef.current.value = "";
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+
     return (
-        <div className="bg-black flex flex-col items-center justify-center flex-1 p-4">
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md lg:max-w-lg xl:max-w-xl">
+        <PageContainer>
+            <FormContainer>
                 <h2 className="text-white text-2xl font-bold mb-6 text-center">
                     Modificación de Hermandad
                 </h2>
                 <form onSubmit={handleSubmit} className="space-y-6">
-                    <div className="relative">
-                        <label
-                            htmlFor="guildname"
-                            className="text-white block mb-1"
-                        >
-                            Nombre de la hermandad:
-                        </label>
-                        <input
-                            type="text"
-                            id="guildname"
-                            ref={guildnameRef}
-                            className="w-full p-3 bg-gray-900 text-white border border-orange-500 rounded-lg placeholder focus:outline-none"
-                            placeholder="Nombre de la hermandad"
-                            required
-                        />
-                    </div>
-                    <div className="relative">
-                        <label
-                            htmlFor="description"
-                            className="text-white block mb-1"
-                        >
-                            Descripción:
-                        </label>
-                        <textarea
-                            id="description"
-                            ref={descriptionRef}
-                            className="w-full p-3 bg-gray-900 text-white border border-orange-500 rounded-lg placeholder focus:outline-none"
-                            placeholder="Descripción (opcional)"
-                            rows="4"
-                        />
-                    </div>
-                    <div className="relative">
-                        <label
-                            htmlFor="avatar"
-                            className="text-white block mb-1"
-                        >
-                            Avatar:
-                        </label>
-                        <input
-                            type="file"
-                            id="avatar"
-                            ref={avatarRef}
-                            className="w-full text-white border border-orange-500 rounded-lg"
-                        />
-                    </div>
-                    <div>
-                        <button
-                            type="submit"
-                            className="w-full py-3 bg-orange-600 text-white font-bold rounded-lg hover:bg-orange-700 transition-colors"
-                        >
-                            Modificar Hermandad
-                        </button>
-                    </div>
+                    <TextInput
+                        id="guildname"
+                        label="Nombre de la hermandad:"
+                        placeholder="Nombre de la hermandad"
+                        ref={guildnameRef}
+                        required
+                    />
+                    <Textarea
+                        id="description"
+                        label="Descripción:"
+                        placeholder="Descripción (opcional)"
+                        ref={descriptionRef}
+                        rows={4}
+                    />
+                    <FileInput id="avatar" label="Avatar:" ref={avatarRef} />
+                    <Button type="submit">Modificar Hermandad</Button>
                 </form>
-            </div>
-        </div>
+            </FormContainer>
+        </PageContainer>
     );
-
 };
 
 export default EditGuildPage;
