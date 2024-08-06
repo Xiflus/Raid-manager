d12e1b973980462b1fd07b8228bc993c9a31cbb4
Adrián Mora <adrianmoratoledo@hotmail.com>
2024-08-06 17:45:11 +0200

    fixes con stefano niceee

--- client/src/components/CharacterSelectionComponent.jsx
+++ client/src/components/CharacterSelectionComponent.jsx
@@ -5,105 +5,120 @@ import { CharacterContext } from "../context/CharacterContext";
 const { VITE_API_URL } = import.meta.env;
 
 const CharacterSelectionComponent = ({ characters }) => {
-	const { selectedCharacter, characterSelection } = useContext(CharacterContext);
-	const [selectedOption, setSelectedOption] = useState(null);
+  const { selectedCharacter, characterSelection } =
+    useContext(CharacterContext);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-	useEffect(() => {
-		// Formatea los datos para react-select
-		const options = characters.map((character) => ({
-			value: character.id,
-			label: (
-				<div className="flex items-center">
-					{character.character_avatar?.length === 0 ? (
-						<img
-							src={`${VITE_API_URL}/${character?.character_avatar}`}
-							alt={`${character.character_name} avatar`}
-							className="w-8 h-8 rounded-full mr-2"
-						/>
-					) : (
-						<img src="/default-guild.png" alt="default-avatar" className="w-8 h-8 rounded-full mr-2" />
-					)}
-					<span className="text-gray-700">{character.character_name}</span>
-				</div>
-			),
-		}));
+  useEffect(() => {
+    // Formatea los datos para react-select
+    const options = characters.map((character) => ({
+      value: character.id,
+      label: (
+        <div className="flex items-center">
+          {character.character_avatar?.length === 0 ? (
+            <img
+              src={`${VITE_API_URL}/${character?.character_avatar}`}
+              alt={`${character.character_name} avatar`}
+              className="w-8 h-8 rounded-full mr-2"
+            />
+          ) : (
+            <img
+              src="/default-guild.png"
+              alt="default-avatar"
+              className="w-8 h-8 rounded-full mr-2"
+            />
+          )}
+          <span className="text-gray-700">{character.character_name}</span>
+        </div>
+      ),
+    }));
 
-		// Encuentra y selecciona la opción correspondiente si `selectedCharacter` existe
-		if (selectedCharacter) {
-			const currentOption = options.find((option) => option.value === selectedCharacter.id);
-			setSelectedOption(currentOption);
-		}
-	}, [characters, selectedCharacter]);
+    // Encuentra y selecciona la opción correspondiente si `selectedCharacter` existe
+    if (selectedCharacter) {
+      const currentOption = options.find((option) => {
+        return option.value === selectedCharacter[0].id;
+      });
+      setSelectedOption(currentOption);
+    }
+  }, [characters, selectedCharacter]);
 
-	// Cambia el personaje seleccionado mediante el contexto
-	const handleChange = (selectedOption) => {
-		const characterId = selectedOption.value;
-		characterSelection(characterId);
-	};
+  // Cambia el personaje seleccionado mediante el contexto
+  const handleChange = (selectedOption) => {
+    const characterId = selectedOption.value;
+    characterSelection(characterId);
+  };
 
 
-
-	return (
-		<div className="p-4">
-			{characters ? (
-				<div>
-					<label htmlFor="characters" className="block  text-gray-700 text-sm font-bold mb-2"></label>
-					<Select
-						options={characters.map((character) => ({
-							value: character.id,
-							label: (
-								<div className="flex items-center">
-									{character?.character_avatar === null ? (
-										<img src="/default-guild.png" alt="default-avatar" className="w-8 h-8 rounded-full mr-2" />
-									) : (
-										<img
-											src={`${VITE_API_URL}/${character?.character_avatar}`}
-											alt={`${character.character_name} avatar`}
-											className="w-8 h-8 rounded-full mr-2"
-										/>
-									)}
-									<span className=" text-gray-700">{character.character_name}</span>
-								</div>
-							),
-						}))}
-						onChange={handleChange}
-						placeholder="Selecciona un personaje"
-						components={{ Option: CustomOption }}
-						value={selectedOption}
-					/>
-				</div>
-			) : (
-				<div className="text-gray-700">Cargando personajes...</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="p-4">
+      {characters ? (
+        <div>
+          <label
+            htmlFor="characters"
+            className="block  text-gray-700 text-sm font-bold mb-2"
+          ></label>
+          <Select
+            options={characters.map((character) => ({
+              value: character.id,
+              label: (
+                <div className="flex items-center">
+                  {character?.character_avatar === null ? (
+                    <img
+                      src="/default-guild.png"
+                      alt="default-avatar"
+                      className="w-8 h-8 rounded-full mr-2"
+                    />
+                  ) : (
+                    <img
+                      src={`${VITE_API_URL}/${character?.character_avatar}`}
+                      alt={`${character.character_name} avatar`}
+                      className="w-8 h-8 rounded-full mr-2"
+                    />
+                  )}
+                  <span className=" text-gray-700">
+                    {character.character_name}
+                  </span>
+                </div>
+              ),
+            }))}
+            onChange={handleChange}
+            placeholder="Selecciona un personaje"
+            components={{ Option: CustomOption }}
+            value={selectedOption}
+          />
+        </div>
+      ) : (
+        <div className="text-gray-700">Cargando personajes...</div>
+      )}
+    </div>
+  );
 };
 
 const CustomOption = (props) => {
-	const { data, innerRef, innerProps } = props;
-	return (
-		<div ref={innerRef} {...innerProps}>
-			{data.label}
-		</div>
-	);
+  const { data, innerRef, innerProps } = props;
+  return (
+    <div ref={innerRef} {...innerProps}>
+      {data.label}
+    </div>
+  );
 };
 
 CharacterSelectionComponent.propTypes = {
-	characters: PropTypes.arrayOf(
-		PropTypes.shape({
-			id: PropTypes.string,
-			character_name: PropTypes.string,
-			character_class: PropTypes.string,
-			character_avatar: PropTypes.string,
-		})
-	).isRequired,
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      character_name: PropTypes.string,
+      character_class: PropTypes.string,
+      character_avatar: PropTypes.string,
+    })
+  ).isRequired,
 };
 CustomOption.propTypes = {
-	data: PropTypes.shape({
-		label: PropTypes.node.isRequired,
-	}).isRequired,
-	innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
-	innerProps: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    label: PropTypes.node.isRequired,
+  }).isRequired,
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
+  innerProps: PropTypes.object.isRequired,
 };
 
 export default CharacterSelectionComponent;
--- client/src/pages/GuildPage.jsx
+++ client/src/pages/GuildPage.jsx
@@ -12,95 +12,125 @@ import { CharacterContext } from "../context/CharacterContext";
 import { AuthContext } from "../context/AuthContext";
 
 const GuildPage = () => {
-	const { authUser } = useContext(AuthContext);
-	!authUser && window.location.replace("/login");
-	const { selectedCharacter } = useContext(CharacterContext);
-	const { guildId } = useParams();
-	const [guild, setGuild] = useState(null);
-	const [loading, setLoading] = useState(true);
-	const { posts, prevPage, nextPage, currentPage, totalPages, totalPosts, goToPage } = usePosts();
+  const { authUser } = useContext(AuthContext);
+  !authUser && window.location.replace("/login");
+  const { selectedCharacter } = useContext(CharacterContext);
+  const { guildId } = useParams();
+  const [guild, setGuild] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const {
+    posts,
+    prevPage,
+    nextPage,
+    currentPage,
+    totalPages,
+    totalPosts,
+    goToPage,
+  } = usePosts();
 
-	useEffect(() => {
-		// Si no hay personaje seleccionado, no intentar cargar la guild
-		if (!selectedCharacter || selectedCharacter.length === 0) {
-			setLoading(false);
-			return;
-		}
+  useEffect(() => {
+    // Si no hay personaje seleccionado, no intentar cargar la guild
+    if (!selectedCharacter || selectedCharacter.length === 0) {
+      setLoading(false);
+      return;
+    }
 
-		const fetchGuild = async () => {
-			try {
-				const response = await getGuildService(guildId);
-				setGuild(response?.guild[0]);
-			} catch (error) {
-				console.log("GuildPage", error);
-				toast.error("¡Error al cargar la guild!");
-			} finally {
-				setLoading(false);
-			}
-		};
-		fetchGuild();
-	}, [guildId, selectedCharacter]);
+    const fetchGuild = async () => {
+      try {
+        const response = await getGuildService(guildId);
+        setGuild(response?.guild[0]);
+      } catch (error) {
+        console.log("GuildPage", error);
+        toast.error("¡Error al cargar la guild!");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchGuild();
+  }, [guildId, selectedCharacter]);
 
-	// Si no hay personaje seleccionado, mostrar un mensaje
-	if (!selectedCharacter || selectedCharacter.length === 0) {
-		return <p>Todavía no has seleccionado ningún personaje, selecciona uno para ver la información de la hermandad.</p>;
-	}
+  // Si no hay personaje seleccionado, mostrar un mensaje
+  if (!selectedCharacter || selectedCharacter.length === 0) {
+    return (
+      <p>
+        Todavía no has seleccionado ningún personaje, selecciona uno para ver la
+        información de la hermandad.
+      </p>
+    );
+  }
 
-	// Mostrar animación de carga si está cargando
-	if (loading) {
-		return <h1>Cargando...</h1>;
-	}
+  // Mostrar animación de carga si está cargando
+  if (loading) {
+    return <h1>Cargando...</h1>;
+  }
 
-	const characterGuildId = selectedCharacter[0]?.guild_id;
-	const characterRole = selectedCharacter[0]?.role;
-	const characterName = selectedCharacter[0]?.character_name;
+  const characterGuildId = selectedCharacter[0]?.guild_id;
+  const characterRole = selectedCharacter[0]?.role;
+  const characterName = selectedCharacter[0]?.character_name;
 
-	return (
-		<>
-			<div className="">
-				<h1>{guild.name}</h1>
-			</div>
-			<div className="">
-				{guild.avatar?.length > 0 ? <img src={`${VITE_API_URL}/${guild?.avatar}`} alt="" /> : <img src="/default-guild.png" alt="" />}
-			</div>
-			<div className="">
-				<p>
-					<strong>Miembros: </strong>
-					{guild.members}
-				</p>
-				<p>
-					<strong>Descripción: </strong>
-					{guild.description}
-				</p>
-				{!characterGuildId && <JoinRequestButton guildId={guildId} characterName={characterName} />}
+  return (
+    <>
+      {guild && (
+        <>
+          <div className="">
+            <h1>{guild.name}</h1>
+          </div>
+          <div className="">
+            {guild.avatar?.length > 0 ? (
+              <img src={`${VITE_API_URL}/${guild?.avatar}`} alt="" />
+            ) : (
+              <img src="/default-guild.png" alt="" />
+            )}
+          </div>
+          <div className="">
+            <p>
+              <strong>Miembros: </strong>
+              {guild.members}
+            </p>
+            <p>
+              <strong>Descripción: </strong>
+              {guild.description}
+            </p>
+            {!characterGuildId && (
+              <JoinRequestButton
+                guildId={guildId}
+                characterName={characterName}
+              />
+            )}
 
-				{characterGuildId === guild.id && (
-					<>
-						<div>
-							<PostList posts={posts} />
-							<Pagination
-								prevPage={prevPage}
-								nextPage={nextPage}
-								currentPage={currentPage}
-								totalPages={totalPages}
-								totalPosts={totalPosts}
-								goToPage={goToPage}
-							/>
-						</div>
-						<Link to={`/guilds/${guild.id}/posts/create`}>Nuevo post</Link>
-						{characterRole === "staff" && (
-							<>
-								<Link to={`/guilds/${guild.id}/edit`}>Editar Hermandad</Link>
-								<div>
-									<p>Crear un componente para mostrar las solicitudes de unión a un admin</p>
-								</div>
-							</>
-						)}
-					</>
-				)}
-			</div>
-		</>
-	);
+            {characterGuildId === guild.id && (
+              <>
+                <div>
+                  <PostList posts={posts} />
+                  <Pagination
+                    prevPage={prevPage}
+                    nextPage={nextPage}
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    totalPosts={totalPosts}
+                    goToPage={goToPage}
+                  />
+                </div>
+                <Link to={`/guilds/${guild.id}/posts/create`}>Nuevo post</Link>
+                {characterRole === "staff" && (
+                  <>
+                    <Link to={`/guilds/${guild.id}/edit`}>
+                      Editar Hermandad
+                    </Link>
+                    <div>
+                      <p>
+                        Crear un componente para mostrar las solicitudes de
+                        unión a un admin
+                      </p>
+                    </div>
+                  </>
+                )}
+              </>
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
-
 export default GuildPage;
