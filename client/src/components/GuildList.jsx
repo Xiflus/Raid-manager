728da2840e7804ccb0100fa5542e8f996138b2ef
Roberto Panella <tito874@hotmail.com>
2024-08-07 17:30:56 +0200

    arreglos

--- client/src/components/GuildList.jsx
+++ client/src/components/GuildList.jsx
@@ -13,20 +13,20 @@ const GuildList = ({ guilds }) => {
 	}
 	return (
 		<>
-			<h1 className="text-center text-gradient text-3xl sm:text-5xl tracking-wider font-lifeCraft mb-6">
-				Hermandades
-			</h1>
-			<ul className="flex flex-wrap justify-center gap-4">
-				{guilds[0]?.map((guild) => {
-					return (
-						<li key={guild.id} className="flex-grow-0 flex-shrink-0 w-full sm:w-1/2 lg:w-1/3 px-2">
-							<GuildListItem guild={guild} />
-						</li>
-					);
-				})}
-			</ul>
+		  <h1 className="text-center text-gradient text-3xl sm:text-5xl tracking-wider font-lifeCraft mb-6">
+			Hermandades
+		  </h1>
+		  <ul className="flex flex-wrap justify-center gap-4">
+			{guilds[0]?.map((guild) => {
+			  return (
+				<li key={guild.id} className="flex-grow-0 flex-shrink-0 w-72">
+				  <GuildListItem guild={guild} />
+				</li>
+			  );
+			})}
+		  </ul>
 		</>
-	);
-};
+	  );
+	};
 
 export default GuildList;
